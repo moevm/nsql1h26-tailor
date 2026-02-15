49c7fd93ce301e668a064b96b49f97a31f588fb2
Коршков Александр <alek.korshkov@yandex.ru>
2026-02-15 22:37:33 +0300

    Add Hello World to main branch (#1)
    * Initial commit
    
    * added some info in readme and recommended extensions for vscode
    
    * some additional info in README.md
    
    * deleted TODO from README.md
    
    * git files now on top of repo
    
    * add some rules for .gitignore
    
    * migrate to ESLint 10, rewrite eslint, added comments on Russian to tsconfig and added prettier plugin for eslint
    
    * lint readme.md

---
+++ hello_world/eslint.config.ts
@@ -0,0 +1,28 @@
+/**
+ * @file eslint.config.ts
+ * @author Korzik
+ * @module eslintConfig
+ * @description Конфигурационный файл для ESLint.
+ * @license ISC
+ */
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
+])
