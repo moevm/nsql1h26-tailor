0140ea93f597a60f8d843210b826b1e957a549a0
KorzikAlex <lumic.alex@gmail.com>
2026-03-16 23:50:49 +0300

    added naive ui providers for necessary functions

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+/**
+ * @file App.vue
+ * @description Главный компонент приложения,
+ * обеспечивающий глобальные провайдеры и маршрутизацию.
+ * @author @KorzikAlex
+ */
+import {
+  NConfigProvider,
+  NDialogProvider,
+  NGlobalStyle,
+  NLoadingBarProvider,
+  NMessageProvider,
+  NNotificationProvider,
+  dateRuRU,
+  ruRU,
+} from 'naive-ui';
+</script>
 
 <template>
-  <router-view />
+  <n-config-provider :locale="ruRU" :date-locale="dateRuRU">
+    <n-global-style />
+    <n-loading-bar-provider>
+      <n-message-provider>
+        <n-dialog-provider>
+          <n-notification-provider>
+            <router-view />
+          </n-notification-provider>
+        </n-dialog-provider>
+      </n-message-provider>
+    </n-loading-bar-provider>
+  </n-config-provider>
 </template>
 
 <style scoped lang="scss"></style>
