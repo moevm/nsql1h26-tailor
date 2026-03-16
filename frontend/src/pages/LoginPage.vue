9b3ee3c522f72585164da853712f353d788c2a99
KorzikAlex <lumic.alex@gmail.com>
2026-03-16 23:49:07 +0300

    added LoginPage

---
+++ frontend/src/pages/LoginPage.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+/**
+ * @file LoginPage.vue
+ * @description Страница входа в систему
+ * @author @KorzikAlex
+ */
+import { LoginCard } from '@/components/cards';
+</script>
+
+<template>
+  <div class="login-page">
+    <LoginCard />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.login-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
