f472dc773c6aa24d70537bf85a6d4f4b1eaa8390
KorzikAlex <lumic.alex@gmail.com>
2026-03-16 23:49:19 +0300

    added LoginPage to router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -8,8 +8,12 @@ import { type RouterOptions, createRouter, createWebHistory } from 'vue-router';
 const routes: RouterOptions['routes'] = [
   {
     path: '/',
-    name: 'Home',
-    component: () => import('@/pages/HelloWorld.vue'),
+    redirect: '/login',
+  },
+  {
+    path: '/login',
+    name: 'Вход в систему',
+    component: () => import('@/pages/LoginPage.vue'),
   },
 ];
 
