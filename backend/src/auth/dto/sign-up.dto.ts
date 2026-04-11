cd6fdb4cbdd708a091a696332bf3e42aa6631c47
Никита Пивоев <nikita.pivoev@yandex.ru>
2026-04-11 23:30:40 +0300

    feat: add auth with jwt and bcrypt

--- backend/src/auth/dto/sign-up.dto.ts
+++ backend/src/auth/dto/sign-up.dto.ts
@@ -1,1 +1,25 @@
-export class SignUpDto {}
+import { IsEmail, IsOptional, IsPhoneNumber, IsString } from 'class-validator';
+
+export class SignUpDto {
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsOptional()
+  @IsString()
+  patronymic?: string;
+
+  @IsOptional()
+  @IsString()
+  @IsPhoneNumber()
+  phone?: string;
+
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+}
