fb7e4b9602d8294e1229a1aa7a4af0dd103540fd
Никита Пивоев <nikita.pivoev@yandex.ru>
2026-04-11 23:29:51 +0300

    feat: activate validation and cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,21 +1,32 @@
-/**
- * @file main.ts
- * @description Точка входа в приложение.
- * @author @KorzikAlex
- */
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 
 import { AppModule } from './app.module';
 
-/**
- * @function bootstrap
- * @description Функция для запуска приложения.
- */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(helmet()); // Используем Helmet для обеспечения безопасности HTTP-заголовков
-  app.enableCors(); // Включаем CORS для разрешения запросов с других доменов
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  app.use(helmet());
+
+  app.enableCors({
+    origin: [
+      // Frontend
+      'http://127.0.0.1:8080',
+      'http://localhost:8080',
+      // MongoDB
+      'http://127.0.0.1:27017',
+      'http://localhost:27017',
+    ],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
