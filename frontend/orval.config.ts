90d8195460341f532350a78ca6c23c91f2eb63a7
KorzikAlex <lumic.alex@gmail.com>
2026-04-15 23:00:31 +0300

    Убрал tanstack vue query (слишком сложный, никто в нём копаться не будет)

--- frontend/orval.config.ts
+++ frontend/orval.config.ts
@@ -1,3 +1,6 @@
+/**
+ * @description Конфигурация для генерации API-клиента на основе OpenAPI
+ */
 import { resolve } from 'path';
 
 import { defineConfig } from 'orval';
@@ -8,15 +11,13 @@ export default defineConfig({
       target: resolve(__dirname, '../openapi.json'),
     },
     output: {
-      client: 'vue-query',
+      client: 'axios',
       httpClient: 'axios',
       clean: true,
       mode: 'tags',
       target: 'src/api',
       schemas: './src/api/schemas',
-      baseUrl: {
-        getBaseUrlFromSpecification: true,
-      },
+      baseUrl: process.env.VITE_API_URL,
     },
     hooks: {
       afterAllFilesWrite: ['prettier --write "src/api/**/*.{ts,tsx}"'],
