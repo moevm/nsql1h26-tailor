fe720a7c2af28e4a81e4212ab235b9971ee0f36b
KorzikAlex <lumic.alex@gmail.com>
2026-04-22 13:49:32 +0300

    Отключил eslint неиспользуемые переменные (потому что пока заглушки)

--- backend/src/import/import.service.ts
+++ backend/src/import/import.service.ts
@@ -4,5 +4,6 @@ import { ImportDatabaseDto } from './dto/import-database.dto';
 
 @Injectable()
 export class ImportService {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async importDatabase(importDatabaseDto: ImportDatabaseDto) {}
 }
