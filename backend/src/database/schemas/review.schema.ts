3b66f2c57408a0d932af3f1261f1b59c5ae940a9
KorzikAlex <lumic.alex@gmail.com>
2026-04-22 11:56:00 +0300

    Были ошибки компиляции, потому что поля в классе не были явно не проинициализированы (т.к. нет конструктора)

--- backend/src/database/schemas/review.schema.ts
+++ backend/src/database/schemas/review.schema.ts
@@ -6,22 +6,22 @@ export type ReviewDocument = HydratedDocument<Review>;
 @Schema()
 export class Review {
   @Prop({ type: mongoose.Schema.Types.ObjectId, required: true })
-  orderId: mongoose.Types.ObjectId;
+  orderId!: mongoose.Types.ObjectId;
 
   @Prop({ type: mongoose.Schema.Types.ObjectId, default: null })
-  tailorId: mongoose.Types.ObjectId;
+  tailorId?: mongoose.Types.ObjectId;
 
   @Prop({ type: mongoose.Schema.Types.ObjectId, required: true })
-  authorId: mongoose.Types.ObjectId;
+  authorId!: mongoose.Types.ObjectId;
 
   @Prop({ min: 1, max: 5, required: true })
-  rating: number;
+  rating!: number;
 
   @Prop({ required: true })
-  comment: string;
+  comment!: string;
 
   @Prop({ default: Date.now })
-  createdAt: Date;
+  createdAt!: Date;
 }
 
 export const ReviewSchema = SchemaFactory.createForClass(Review);
