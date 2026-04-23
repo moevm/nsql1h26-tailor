5baed4db9f68eda85be42416210fa215ebd167fb
KorzikAlex <lumic.alex@gmail.com>
2026-04-23 23:01:40 +0300

    Кнопка выхода из сессии пользователя

--- frontend/src/components/buttons/LogOutButton.vue
+++ frontend/src/components/buttons/LogOutButton.vue
@@ -1,16 +1,26 @@
 <script setup lang="ts">
-import { LogOutRound } from '@vicons/material';
-import { NButton, NIcon } from 'naive-ui';
-import { useRouter } from 'vue-router';
 import { useAuthStore } from '@/stores';
-
-function handleLogOut() {
-  authStore.logout();
-  router.push('/login');
-}
+import { LogOutRound } from '@vicons/material';
+import { NButton, NEl, NIcon, useDialog, useMessage } from 'naive-ui';
+import { useRouter } from 'vue-router';
 
 const router = useRouter();
 const authStore = useAuthStore();
+const dialog = useDialog();
+const message = useMessage();
+
+function handleLogOut() {
+  dialog.info({
+    title: 'Выйти из аккаунта?',
+    positiveText: 'Да',
+    negativeText: 'Нет',
+    onPositiveClick: () => {
+      authStore.logout();
+      router.push('/login');
+      message.success('Вы успешно вышли из аккаунта');
+    },
+  });
+}
 </script>
 
 <template>
@@ -20,8 +30,16 @@ const authStore = useAuthStore();
         <LogOutRound />
       </n-icon>
     </template>
-    Выход
+    <template #default>
+      <n-el tag="span" class="button-text"> Выход </n-el>
+    </template>
   </n-button>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.button-text {
+  @media (max-width: 600px) {
+    display: none;
+  }
+}
+</style>
