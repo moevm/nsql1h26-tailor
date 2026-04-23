4ecbb3afff1fa67c717b29085f03fba697e7706b
KorzikAlex <lumic.alex@gmail.com>
2026-04-23 23:32:21 +0300

    Добавлена карточка для несуществующих страниц

---
+++ frontend/src/components/cards/NotFoundCard.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { NButton, NCard, NFlex } from 'naive-ui';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+</script>
+
+<template>
+  <n-card
+    title="Ошибка 404: Страница не найдена"
+    bordered
+    class="not-found-card"
+  >
+    <n-flex vertical>
+      Извините, страница, которую вы ищете, не существует. Пожалуйста, проверьте
+      URL или вернитесь на главную страницу.
+      <n-flex justify="center" align="center">
+        <n-button type="primary" @click="router.push('/home')">
+          Вернуться
+        </n-button>
+      </n-flex>
+    </n-flex>
+  </n-card>
+</template>
+
+<style lang="scss" scoped>
+.not-found-card {
+  width: 500px;
+
+  @media (max-width: 500px) {
+    width: 100%;
+  }
+}
+</style>
