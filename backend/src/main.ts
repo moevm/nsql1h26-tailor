b3f2559eea6c0c004a7060269c108672a8118c96
KorzikAlex <lumic.alex@gmail.com>
2026-04-23 23:41:52 +0300

    Добавил callback, чтобы было понятно, запустился ли корректно сервер

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -24,7 +24,11 @@ async function bootstrap() {
     ],
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+
+  await app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
 }
 
 void bootstrap();
