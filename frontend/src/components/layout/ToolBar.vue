e8cf2f4609b7b0e490fc5d4b82a5b5c25f2b692d
Danil Otmakhov <danil.otmakhov@gmail.com>
2026-04-27 20:25:11 +0300

    feat: add back button

--- frontend/src/components/layout/ToolBar.vue
+++ frontend/src/components/layout/ToolBar.vue
@@ -1,24 +1,33 @@
 <script setup lang="ts">
-import { LogOutButton, ProfileButton } from '@/components/buttons';
+import { BackButton, LogOutButton, ProfileButton } from '@/components/buttons';
 import { useAuthStore } from '@/stores';
 import { NFlex, NLayoutHeader } from 'naive-ui';
 import { computed } from 'vue';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 
 const route = useRoute();
+const router = useRouter();
 const authStore = useAuthStore();
 
 const title = computed(() => route.name);
+const canNavigateBack = computed(() => Boolean(route.meta?.canNavigateBack));
 const profileInfo = computed(() => ({
   firstName: authStore.user?.name?.firstName ?? 'Пользователь',
   lastName: authStore.user?.name?.lastName ?? '',
 }));
+
+function navigateUp() {
+  router.back();
+}
 </script>
 
 <template>
   <n-layout-header bordered>
     <n-flex justify="space-between" align="center" class="layout-header">
-      <h1>{{ title }}</h1>
+      <n-flex align="center">
+        <BackButton :can-navigate-back="canNavigateBack" :on-navigate-up="navigateUp" />
+        <h1>{{ title }}</h1>
+      </n-flex>
       <n-flex justify="space-between" align="center">
         <ProfileButton
           :first-name="profileInfo.firstName"
