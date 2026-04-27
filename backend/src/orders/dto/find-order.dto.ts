09045816a28a6e3205fae52934363f12f3ec1273
Никита Пивоев <nikita.pivoev@yandex.ru>
2026-04-27 21:28:35 +0300

    feat: add filters for orders

---
+++ backend/src/orders/dto/find-order.dto.ts
@@ -0,0 +1,23 @@
+import { IsDateString, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class FindOrderDto {
+  @IsOptional()
+  @IsDateString()
+  startDate?: string;
+
+  @IsOptional()
+  @IsDateString()
+  endDate?: string;
+
+  @IsOptional()
+  @IsNumber()
+  minPrice?: number;
+
+  @IsOptional()
+  @IsNumber()
+  maxPrice?: number;
+
+  @IsOptional()
+  @IsString()
+  status?: string;
+}
--- backend/src/orders/orders.controller.ts
+++ backend/src/orders/orders.controller.ts
@@ -14,6 +14,7 @@ import { DeleteResult, UpdateResult } from 'mongoose';
 
 import { Order } from '../database/schemas/order.schema';
 import { CreateOrderDto } from './dto/create-order.dto';
+import { FindOrderDto } from './dto/find-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 import { OrdersService } from './orders.service';
 
@@ -24,8 +25,8 @@ export class OrdersController {
   @UseGuards(RolesGuard)
   @Roles(['customer', 'tailor', 'manager'])
   @Get()
-  findAll(): Promise<Order[]> {
-    return this.ordersService.getAllOrders();
+  findAll(findOrderDto: FindOrderDto): Promise<Order[]> {
+    return this.ordersService.getAllOrdersFiltered(findOrderDto);
   }
 
   @UseGuards(RolesGuard)
