733946bbc28f366affb148c09b4aeb80e010445b
Никита Пивоев <nikita.pivoev@yandex.ru>
2026-05-10 15:23:42 +0300

    feat: add update endpoint for profile

---
+++ backend/src/users/users.module.ts
@@ -0,0 +1,13 @@
+import { UserSchema } from '@/database/schemas/user.schema';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+  controllers: [UsersController],
+  providers: [UsersService],
+})
+export class UsersModule {}
